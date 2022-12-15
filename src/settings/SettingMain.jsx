9c5f8dac0346d9b8effb99d17fe1dd9fd6f5fd58
PC-10121024\hoon <imode7@naver.com>
2022-12-15 17:34:52 +0900

    main 헤더 작업중

--- src/settings/SettingMain.jsx
+++ src/settings/SettingMain.jsx
@@ -1,14 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function SettingMain() {
-  return <View style={styles.container}>test</View>;
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  settingContainer: {
+    width: "98%",
+    height: "98%",
     backgroundColor: "aquamarine",
     alignItems: "center",
     justifyContent: "center",
   },
 });
+
+export default function SettingMain() {
+  return (
+    <View style={styles.settingContainer}>
+      {/*운동 설정에 대한 큰 박스*/}
+      <View style={{ width: "100%" }}>
+        {/*그 안의 돋보기가 속해있는 큰 박스*/}
+        <View style={{ width: "100%" }}>
+          {/*운동설정, 편집이 들어가 있는 박스*/}
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text></Text>
+            <Text>운동설정</Text>
+            <Text>편집</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
