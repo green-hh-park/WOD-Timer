4f0ab99f102b2e6f0c0d8b5ade5a6d1c76aaff7e
PC-10121024\hoon <imode7@naver.com>
2022-12-15 16:35:09 +0900

    Directory 구조 (src -> Settings(내가 필요한 폴더))부분 설명하기위해 올림. 개발중

---
+++ src/settings/SettingMain.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SettingMain() {
+  return <View style={styles.container}>test</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "aquamarine",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
